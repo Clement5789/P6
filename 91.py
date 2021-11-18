b22ca3fb2be14ab12d9f300a11f19cb2d75a69ab
Clement5789 <90597740+Clement5789@users.noreply.github.com>
2021-11-18 17:23:23 +0100

    Add files via upload

---
+++ 91.py
@@ -0,0 +1,8 @@
+a=int(input("un entier positif : "))
+while a>1:
+    if a%2==1:
+        a=a*3+1
+    else:
+        a=a//2
+    print(a)
+        
---
+++ ex 90.py
@@ -0,0 +1,7 @@
+def caca(r):
+    e=0
+    while r>1:
+      r=r/10
+      e=e+1
+    return(e)
+
